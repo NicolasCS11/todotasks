0ea9492fff8e98588e526741af3fbea992352c26
NicolasCS11 <nicolascano1@hotmail.com>
2022-06-01 19:40:56 -0500

    createtask

---
+++ server.js
@@ -0,0 +1,20 @@
+const api = require('./api');
+
+const bodyParser = require('body-parser');
+
+
+const express = require('express');
+const port = process.env.PORT || 3000;
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.json());
+
+app.listen(port, function () {
+    console.log("Server is listening at port: " + port);
+});
+
+app.get('/', function (req, res) {
+    res.send("hello world");
+});
+
+app.use('/api', api);
